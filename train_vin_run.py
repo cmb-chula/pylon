6b2731ff14c641d76f62c134adb7da5ad44728b6
Konpat Preechakul <the.akita.ta@gmail.com>
2021-06-06 18:02:43 +0700

    update

--- train_vin_run.py
+++ train_vin_run.py
@@ -4,17 +4,17 @@ if __name__ == "__main__":
     confs = []
 
     # main experiments
-    # for seed in [0]:
-    #     confs += vin_baseline(seed, 256)
-    #     confs += vin_li2018(seed, 256)
-    #     confs += vin_fpn(seed, 256)
-    #     confs += vin_pylon(seed, 256)
+    for seed in [0]:
+        confs += vin_baseline(seed, 256)
+        confs += vin_li2018(seed, 256)
+        confs += vin_fpn(seed, 256)
+        confs += vin_pylon(seed, 256)
 
     # transfer learning
-    # for seed in [0]:
-    #     confs += vin_baseline_transfer(seed, 256)
-    #     confs += vin_pylon_transfer(seed, 256)
-    #     confs += vin_pylon_transfer_two_phase(seed, 256)
+    for seed in [0]:
+        confs += vin_baseline_transfer(seed, 256)
+        confs += vin_pylon_transfer(seed, 256)
+        confs += vin_pylon_transfer_two_phase(seed, 256)
 
     debug = False
     multiprocess_map(
